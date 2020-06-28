3ab0af96af43209110e23d77496a4ec039907381
Olivia Clausi <oliviaclausi@gmail.com>
2020-06-28 17:12:56 -0400

    initial commit (https://github.com/csharpfritz/aspnetcore-app-workshop)

---
+++ BackEnd/Models/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BackEnd.Models
+{
+    public class ApplicationDbContext : DbContext // colon means inherit from this other class
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+
+        {
+
+        }
+
+        public DbSet<Speaker> Speakers { get; set; }
+    }
+}
